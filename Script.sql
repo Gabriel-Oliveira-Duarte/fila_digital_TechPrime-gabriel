31143583b047eaecbb232b72220ac8618ad7bdc8
Gabriel-Oliveira-Duarte <gabrieloliveiraduarte14@gmail.com>
2026-03-24 23:41:42 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -186,7 +186,7 @@ CREATE TABLE fila_cliente (
   data_fim_atendimento DATETIME NULL,
   data_saida DATETIME NULL,
 
-  /* mantido porque existe um update antigo no seu código usando data_fim */
+
   data_fim DATETIME NULL,
 
   CONSTRAINT fk_fc_fila
@@ -209,9 +209,7 @@ CREATE TABLE password_reset (
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
 
-/* =========================
-   ÍNDICES
-========================= */
+
 
 CREATE INDEX idx_cliente_status
   ON cliente (status);
@@ -270,42 +268,3 @@ CREATE INDEX idx_password_reset_email_id
 CREATE INDEX idx_password_reset_expires
   ON password_reset (expires_at);
   
-  INSERT INTO estabelecimento (
-  nome,
-  cnpj,
-  categoria,
-  cidade,
-  estado,
-  telefone,
-  cep,
-  numero,
-  complemento,
-  logradouro,
-  bairro,
-  cidade_end,
-  uf,
-  latitude,
-  longitude,
-  raio_alerta,
-  email,
-  senha
-) VALUES (
-  'SENAI Betim - Maria Madalena Nogueira',
-  '00000000000001',
-  'OUTROS',
-  'Betim',
-  'MG',
-  '31991722376',
-  '32600075',
-  '55',
-  NULL,
-  'Avenida Amazonas',
-  'Centro',
-  'Betim',
-  'MG',
-  -19.96475640,
-  -44.19351800,
-  0,
-  'senai@teste.com',
-  '5b09b9851902b584b714a1ebc0eb06bcb142ac64f754669a34a4791ede1cd415'
-);
